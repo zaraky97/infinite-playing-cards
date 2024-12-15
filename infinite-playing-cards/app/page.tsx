2e4a38d27e09b3278b790e6f0f5ffd417329d19d
DESKTOP-JTH1EPQ\sasag <yuya.sasagawa@item-shop.jp>
2024-12-15 23:14:12 +0900

    タイトル変更

--- infinite-playing-cards/app/page.tsx
+++ infinite-playing-cards/app/page.tsx
@@ -125,7 +125,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="text-3xl">
-        infinite-playing-card{" "}
+        無限トランプ{" "}
         <div className="text-center text-2xl">試行回数:{matchUpCount}</div>
       </div>
 
